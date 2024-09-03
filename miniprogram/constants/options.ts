b3744ce52bd88940f44c6887440c5987bbdcb4b7
flyingideal <flyingideal_yan@163.com>
2024-09-04 01:05:20 +0800

    feat: 完成项目创建接口调试

---
+++ miniprogram/constants/options.ts
@@ -0,0 +1,49 @@
+export const projectTypeOptions = [
+  {
+    id: 1,
+    name: "旅游"
+  },
+  {
+    id: 2,
+    name: "团队活动"
+  },
+  {
+    id: 0,
+    name: "其他"
+  }
+]
+
+export const projectDetailTypeOptions = [
+  {
+    id: 1,
+    name: "餐饮"
+  },
+  {
+    id: 2,
+    name: "交通"
+  },
+  {
+    id: 3,
+    name: "住宿"
+  },
+  {
+    id: 4,
+    name: "门票"
+  },
+  {
+    id: 5,
+    name: "购物"
+  },
+  {
+    id: 6,
+    name: "零食"
+  },
+  {
+    id: 7,
+    name: "医疗"
+  },
+  {
+    id: 0,
+    name: "其他"
+  }
+]
